6a63535901abeff8b012958835079edf6c6cb6c1
Ricardo Vitor <ricardovdev@hotmail.com>
2024-08-26 14:09:35 -0300

    Adding CRUD modulation example

--- modulation-final-exercise/functions/update-function.js
+++ modulation-final-exercise/functions/update-function.js
@@ -1,2 +1,26 @@
 const {tutor} = require('../src/database');
 
+function updateTutorByName(nome, renomear) {
+    tutor[tutor.indexOf(tutor.find(tutor => tutor.nome == nome))].nome = renomear;
+};
+
+function updateTutorByCpf(cpf, trocaCpf) {
+    tutor[tutor.indexOf(tutor.find(tutor => tutor.cpf == cpf))].cpf = trocaCpf;
+};
+
+function updateTutorByContact(contato, trocaContato) {
+    tutor[tutor.indexOf(tutor.find(tutor => tutor.contato == contato))].contato = trocaContato;
+};
+
+function updateTutorByEndereco(endereco, novoEndereco) {
+    tutor[tutor.indexOf(tutor.find(tutor => tutor.endereco == endereco))].endereco = novoEndereco;
+};
+
+module.exports = {
+    updateTutorByName,
+    updateTutorByCpf,
+    updateTutorByContact,
+    updateTutorByEndereco
+}
+
+
--- modulation-final-exercise/index.js
+++ modulation-final-exercise/index.js
@@ -1,7 +1,12 @@
 const {tutor} = require("./src/database");
 const {addTutor} = require("./functions/add-function");
 const {deleteTutorByCpf} = require("./functions/delete-function");
-const {exibirTutor} = require("./functions/read-function"); 
+const {exibirTutor} = require("./functions/read-function");
+const controllerUpdate = require("./functions/update-function"); // controller = controla função update
+// const {updateTutorByName} = require("./functions/update-function"); 
+// const {updateTutorByCpf} = require("./functions/update-function"); 
+// const {updateTutorByContact} = require("./functions/update-function"); 
+// const {updateTutorByEndereco} = require("./functions/update-function"); 
 
 addTutor("ruan", "2434324234", "23434324@hotmail.com", {
     cidade:"recife",
@@ -13,4 +18,7 @@ addTutor("ruan", "2434324234", "23434324@hotmail.com", {
 exibirTutor();
 
 deleteTutorByCpf("123.456.789.10");
+exibirTutor();
+
+controllerUpdate.updateTutorByName("John", "Lucio");
 exibirTutor();
